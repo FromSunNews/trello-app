e40c7553522796a0f10eddf319bc643de89818f7
FromSunNews <phuongtu590@gmail.com>
2022-08-20 15:42:44 +0700

    Refactoring architecture!!

--- src/App.js
+++ src/App.js
@@ -1,102 +1,15 @@
-
+import React from 'react';
 import './App.scss'
-
+// custom component
+import AppBar from 'components/AppBar/AppBar';
+import BoardBar from 'components/BoardBar/BoardBar';
+import BoardContent from 'components/BoardContent/BoardContent';
 function App() {
   return (
     <div className="trello-app-master">
-      <nav className='navbar app'>App Bar</nav>
-      <nav className='navbar board'>Board Bar</nav>
-      <div className="board-columns">
-        <div className='column'>
-          <header>Brain Storm</header>
-          <ul>
-            <li>
-              <img src="https://cdn.tgdd.vn/Files/2019/01/01/1142002/s8ori_800x600.jpg" alt="fromsunnews-alt-img" />
-              Title : FromSunNews
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className='column'>
-          <header>Brain Storm</header>
-          <ul>
-            <li>
-              <img src="https://luv.vn/wp-content/uploads/2021/08/hinh-anh-gai-xinh-16.jpg" alt="fromsunnews-alt-img" />
-              Title : FromSunNews
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className='column'>
-          <header>Brain Storm</header>
-          <ul>
-            <li>
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxWtTEoJ_zRgKiqs67Dv4zmlAQA3lcWgYJrVA-pxO10SN_91zxzwxF0_mmCgI6SvVTX2E&usqp=CAU" alt="fromsunnews-alt-img" />
-              Title : FromSunNews
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className='column'>
-          <header>Brain Storm</header>
-          <ul>
-            <li>
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXKAVAMdFatxJP5L12TZMbd8ls95i__omLsBLH_OC4&s" alt="fromsunnews-alt-img" />
-              Title : FromSunNews
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className='column'>
-          <header>Brain Storm</header>
-          <ul>
-            <li>
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXKAVAMdFatxJP5L12TZMbd8ls95i__omLsBLH_OC4&s" alt="fromsunnews-alt-img" />
-              Title : FromSunNews
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
